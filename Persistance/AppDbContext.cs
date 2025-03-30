d82c4d389a6231bcb3d72ffed5bb3109e67f1531
sam03041993 <60146725+sam03041993@users.noreply.github.com>
2025-03-31 00:39:25 +0530

    Initial commit

---
+++ Persistance/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistance
+{
+    public class AppDbContext(DbContextOptions dbContextOptions) : DbContext(dbContextOptions)
+    {
+        public required DbSet<Activity> Activities { get; set; }
+    }
+}
---
+++ Reactivities/Controllers/ActivitiesController.cs
@@ -0,0 +1,27 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistance;
+
+namespace Reactivities.Controllers
+{
+    public class ActivitiesController(AppDbContext appDbContext) : BaseApiController
+    {
+        private readonly AppDbContext _context = appDbContext;
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivityDetail(string id)
+        {
+            var activity = await _context.Activities.FindAsync(id);
+            if (activity == null) return NotFound();
+
+            return Ok(activity);
+        }
+    }
+}
