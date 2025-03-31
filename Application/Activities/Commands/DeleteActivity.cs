673fbb48f1c6ebcc0a2577d19baaeb6fbc53cfab
sam03041993 <60146725+sam03041993@users.noreply.github.com>
2025-03-31 19:03:26 +0530

    added mediator and mapper for activity

---
+++ Application/Activities/Commands/DeleteActivity.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Persistance;
+
+namespace Application.Activities.Commands
+{
+    public class DeleteActivity
+    {
+        public class Command : IRequest
+        {
+            public required string Id { get; set; }
+        }
+
+        public class Handler(AppDbContext context) : IRequestHandler<Command>
+        {
+            public async Task Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await context.Activities.FindAsync([request.Id], cancellationToken);
+                if (activity == null) throw new Exception("Activity not found");
+
+                context.Activities.Remove(activity);
+
+                await context.SaveChangesAsync(cancellationToken);
+            }
+        }
+    }
+}
--- Reactivities/Controllers/ActivitiesController.cs
+++ Reactivities/Controllers/ActivitiesController.cs
@@ -1,27 +1,44 @@
-﻿using Domain;
+﻿using Application.Activities.Commands;
+using Application.Activities.Queries;
+using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Persistance;
 
 namespace Reactivities.Controllers
 {
-    public class ActivitiesController(AppDbContext appDbContext) : BaseApiController
+    public class ActivitiesController() : BaseApiController
     {
-        private readonly AppDbContext _context = appDbContext;
-
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _context.Activities.ToListAsync();
+            return await Mediator.Send(new GetActivityList.Query()); //_context.Activities.ToListAsync();
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivityDetail(string id)
         {
-            var activity = await _context.Activities.FindAsync(id);
-            if (activity == null) return NotFound();
+            return await Mediator.Send(new GetActivityDetails.Query { Id = id });
+        }
 
-            return Ok(activity);
+        [HttpPost]
+        public async Task<ActionResult<string>> CreateActivity(Activity activity)
+        {
+            return await Mediator.Send(new CreateActivity.Command { Activity = activity });
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<string>> EditActivity(Activity activity)
+        {
+            await Mediator.Send(new EditActivity.Command { Activity = activity });
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteActivity(string id)
+        {
+            await Mediator.Send(new DeleteActivity.Command { Id = id });
+            return NoContent();
         }
     }
 }
